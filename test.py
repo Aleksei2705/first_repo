2c0023f0be28620ac7dbe18f79a0c44fc331abb0
Aleksei2705 <alexpost2705@gmail.com>
2025-03-26 13:21:42 +0500

    first commit

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("!!!")
+def calculate_diameter(radius):
+	return 2 * radius
+
+if __name__ == " __main__ ":
+	try:
+		radius = float(input("Введите радиус окружности"))
+		diameter = calculate_diameter(radius)
+		print("диаметр окружности равен:", diameter)
+	except ValueError:
+		print("Ошибка: необходимо ввести числовое значение.") 
